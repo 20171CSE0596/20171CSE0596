3708052282e6f00d3bef661fb2b2bb4cc1179004
20171CSE0596 <60249485+20171CSE0596@users.noreply.github.com>
2020-01-24 10:54:00 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+int i,a,n;
+printf("enter the number of elements:\t");
+scanf("%d",&n);
+for(i=0;i<=n;i<n)
+{
+printf("enter the array elements:\t");
+scanf("%d",&a[i]);
+for(i=0;i<=n;i<n)
+{
+printf("the array elements are");
+scanf("%d",&a[i]);
+}
+}
+}
+
+
+
+
